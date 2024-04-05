50ce5481122df4769a900884d8c1e2299363c8c4
Alexander Mensing <alex.mensing@cleverfew.co>
2024-04-05 16:26:49 -0400

    evaluate all digits 0 to 9

--- reducer.py
+++ reducer.py
@@ -27,7 +27,7 @@ def reduce_it(parse, size = -1):
     # if the last character is a 0, we may be able to reduce
     if parse[i][-1:] == '0' and i+9 <= length:
       # start here and loop through the next 9 items to see if they are contiguous and correctly sized
-      for j in range(0, 9):
+      for j in range(10):
         if parse[i + j][-1:] == str(j) and not len(parse[i + j]) > size:
           is_contiguous = True
         else:
